ce53a85c6115d99b926af0620bdd71ee5e0baddd
Heonjin Ha <weizen511@gmail.com>
2023-03-14 20:58:21 +0900

    Initial Commit

---
+++ swiftgen-samples/swiftgen_samplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftgen_samplesApp.swift
+//  swiftgen-samples
+//
+//  Created by Heonjin Ha on 2023/03/14.
+//
+
+import SwiftUI
+
+@main
+struct swiftgen_samplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
